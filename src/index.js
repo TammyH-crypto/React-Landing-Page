1c80fc52a4404743d744df6927d25fe5240948fc
TammyH <tmhiggins1986@gmail.com>
2025-01-09 18:14:22 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+import { handleClick } from "./controllers/handleclick";
+import { handleSubmit } from "./controllers/handlesubmit";
+
 const root = createRoot(window.bodyTag);
-root.render()
+root.render(
+
+<main>
+<h1>Rods and reels!</h1>
+    <p>Hello outdoors friends!!!</p>
+    <MyGreeting/>
+    <button onClick={handleClick}>On sale now!</button>
+    <output id="outputTag"></output>
+    <form onSubmit={handleSubmit}>
+      <input type="text">To get a discount</input>
+      <input type="submit"></input>
+</form>   
+
+</main>
+)
