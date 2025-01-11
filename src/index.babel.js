68f57dc2c691fa201c9c7cba9edccebab01a27e5
TammyH <tmhiggins1986@gmail.com>
2025-01-11 14:27:52 -0600

    updated

--- src/index.babel.js
+++ src/index.babel.js
@@ -1,2 +1,16 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+import { handleClick } from "./controllers/handleclick";
+import { handleSubmit } from "./controllers/handlesubmit";
 const root = createRoot(window.bodyTag);
-root.render();
+root.render(/*#__PURE__*/React.createElement("main", null, /*#__PURE__*/React.createElement("h1", null, "Rods and reels!"), /*#__PURE__*/React.createElement("p", null, "Hello outdoors friends!!!"), /*#__PURE__*/React.createElement(MyGreeting, null), /*#__PURE__*/React.createElement("button", {
+  onClick: handleClick
+}, "On sale now!"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("form", {
+  onSubmit: handleSubmit
+}, /*#__PURE__*/React.createElement("input", {
+  type: "text"
+}, "To get a discount"), /*#__PURE__*/React.createElement("input", {
+  type: "submit"
+})), /*#__PURE__*/React.createElement("output", {
+  id: "outputTag"
+})));
--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
-import { createRoot } from "react-dom/client";
 import React from "react";
-import { handleClick } from "./controllers/handleclick";
-import { handleSubmit } from "./controllers/handlesubmit";
+import { createRoot } from "react-dom/client";
 
 const root = createRoot(window.bodyTag);
 root.render(
-
-<main>
-<h1>Rods and reels!</h1>
-    <p>Hello outdoors friends!!!</p>
-    <MyGreeting/>
-    <button onClick={handleClick}>On sale now!</button>
+  <main>
+    <h1 class="m-1 text-center">Rods and reels!</h1>
+    <h2 class="p-2">Hello outdoors friends!!!</h2>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/rhhBPNUXYis?si=q0SyOcgPYlbUj4NE"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+    ></iframe>
+    <br />
+    <p class="san-serif">
+      Hello my fellow fishing enthusiasts! We have 100's of rods and reels on
+      sale right now. Every brand you can think of from abu garcias to zebcos in
+      many different colors to fit every age. We even have ocean poles for you
+      deep sea fishermen who love to go all out. Come check us out and leave
+      your information to recieve an extra discount on our already low prices.
+    </p>
+    <br />
+    <button onClick={() => console.log("click")} class="btn btn-primary">
+      Best price guaranteed!
+    </button>
+    <br />
+    <form onSubmit={() => console.log("discount code 2025")}>
+      <br />
+      <input type="name" />
+      <br />
+      <br />
+      <input type="email" />
+      <br />
+      <br />
+      <input type="number" value={[1, 2, 3] - [4, 5, 6] - [7, 8, 9]} />
+      <br />
+      <br />
+      <input type="submit" class="btn btn-warning" />
+    </form>
     <output id="outputTag"></output>
-    <form onSubmit={handleSubmit}>
-      <input type="text">To get a discount</input>
-      <input type="submit"></input>
-</form>   
-
-</main>
-)
+  </main>
+);
