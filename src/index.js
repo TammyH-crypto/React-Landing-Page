80ac5040b27fa69fe471c2af86c479206c161e07
TammyH <tmhiggins1986@gmail.com>
2025-01-11 16:31:11 -0600

    fixed

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { handleClick } from "./controllers/handleClick";
 const root = createRoot(window.bodyTag);
 root.render(
   <main>
-    <h1 class="m-1 text-center ">Rods and reels!</h1>
+    <h1 className="m-1 text-center ">Rods and reels!</h1>
     <br />
     <iframe
-      class="m-5"
+      className="m-5"
       width="560"
       height="315"
       src="https://www.youtube.com/embed/rhhBPNUXYis?si=q0SyOcgPYlbUj4NE"
@@ -20,7 +20,7 @@ root.render(
       allowFullScreen
     ></iframe>
     <br />
-    <p class="fw-bold fst-italic p-2">
+    <p className="fw-bold fst-italic p-2">
       Hello my fellow fishing enthusiasts! We have 100's of rods and reels on
       sale right now. Every brand you can think of from abu garcias to zebcos in
       many different colors to fit every age. We even have ocean poles for you
@@ -28,8 +28,8 @@ root.render(
       your information to recieve an extra discount on our already low prices.
     </p>
     <br />
-    <p class="p-2">
-      <h3>Abu Garcia Max X baitcast</h3>
+    <h3>Abu Garcia Max X baitcast</h3>
+    <p className="p-2">
       Abu Garcia Max X baitcast Experience better-than-ever Abu Garcia
       engineering for MAXimum performance with the Abu Garcia Max X baitcast
       combo. This combo features a custom designed reel seat with integrated
@@ -37,8 +37,8 @@ root.render(
       ultimate smoothness, and 24-ton graphite construction for ultimate
       sensitivity.
     </p>
-    <p class="p-2">
-      <h3>Zebco Bullet MG Spincast Reel</h3>
+    <h3>Zebco Bullet MG Spincast Reel</h3>
+    <p className="p-2">
       100% performance-driven, the Zebco® Bullet MG Spincast Reel features a
       super-lightweight magnesium body that pairs perfectly with your finest
       rods, for a precision feel and balance that you'll appreciate all day
@@ -56,8 +56,8 @@ root.render(
       Convertible left/right retrieve; pre-spooled with premium 10-lb. mono.
       Manufacturer's 1-year warranty.
     </p>
-    <p class="p-2">
-      <h3>Offshore Angler SeaFire Conventional Rod and Reel Combo</h3>
+    <h3>Offshore Angler SeaFire Conventional Rod and Reel Combo</h3>
+    <p className="p-2">
       Set the water on fire with the Offshore Angler™ SeaFire Conventional Rod
       and Reel Combo. Our SeaFire Conventional Reel is ideal for bottom fishing
       for snapper or grouper. It's also a great trolling reel for tuna, dolphin,
@@ -73,8 +73,8 @@ root.render(
       comfortable EVA handle and graphite gimbal with cover work well with rod
       holders and fighting belts.
     </p>
-    <p class="p-2">
-      <h3>2-Piece Saltwater Spinning Fishing Rod Offshore Heavy Graphite</h3>
+    <h3>2-Piece Saltwater Spinning Fishing Rod Offshore Heavy Graphite</h3>
+    <p className="p-2">
       2-Piece Saltwater Spinning Fishing Rod Offshore Heavy Graphite Portable
       Travel Description: Fiblink 2-Piece Saltwater Spinning fishing Rod
       offshore Carbon Portable Fishing Rod At Fiblink Fishing Tackle we deliver
@@ -91,9 +91,9 @@ root.render(
       Pieces: 2 | Power: Medium Heavy | Guides: 7+1 | Line Weight: 15-30Lb |
       Lure Wei
     </p>
-    <button onClick={handleClick} class="btn btn-primary p-2">
-        Best price guaranteed!
-      </button>
+    <button onClick={handleClick} className="btn btn-primary p-2">
+      Best price guaranteed!
+    </button>
     <br />
     <form onSubmit={handleSubmit}>
       <br />
@@ -112,7 +112,7 @@ root.render(
       />
       <br />
       <br />
-      <input type="submit" class="btn btn-warning" />
+      <input type="submit" className="btn btn-warning" />
     </form>
     <output id="outputTag"></output>
   </main>
